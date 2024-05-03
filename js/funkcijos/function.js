0c0564651060732907271cf747ef70771ba0958b
JurguteJak <jurgutejak@gmail.com>
2024-05-03 13:19:13 +0300

    bandymas

--- js/funkcijos/function.js
+++ js/funkcijos/function.js
@@ -6,7 +6,8 @@ FUNCTION - funkcija, perpanaudojama logika
 Input => Function() => Output
 */
 
-function funkcijosPavadinimas() {}
+function funkcijosPavadinimas() {
+}
 
 const a = funkcijosPavadinimas();
 
@@ -32,22 +33,56 @@ console.log(giveMeFive());
 /*
 Input:
 name: Jonas
-surname: Jonnaitis
+surname: Jonaitis
 
 Output:
 Laba diena, as esu Jonas Jonaitis!
+
+Laba diena, as esu Maryte Marytaite!
+Laba diena, as esu Petras Petraitis!
+Laba diena, as esu Ona Onaityte!
 */
+
+function intro (name, surname) {
+    return 'Laba diena, as esu ' + name + ' ' + surname + '!';
+    return 'Laba diena, as esu VARDAS PAVARDE!';
+}
+
+console.log(intro('Jonas', 'Jonaitis'));
+console.log(intro('Maryte', 'Marytaite'));
+console.log(intro('Petras', 'Petraitis'));
+console.log(intro('Ona', 'Onaityte'));
 
 /*
 Input:
-- pirmas skaicius =5
+- pirmas skaicius = 5
 - antras skaicius = 7
 
 Output:
 35
 */
 
-
 function multiply() {
-    
+    return 0;
 }
+
+console.log(multiply());
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+console.log(multiply(5, 7));
+console.log(multiply(-2, 8));
+console.log(multiply(3.14, 2));
+
+//Mano vardas yra Jonas, man 99 metai ir as megstu cepelinus.
+//Mano vardas yra Maryte, man 88 metai ir as megstu balionus.
+function iLike(name, age, item) {
+    return 'Mano vardas yra ' + name + ', man ' + age + ' metai ir as megstu ' + item + '.';
+    //return 'Mano vardas yra VARDAS, man XX metai ir as megstu DALYKUS';
+}
+
+console.log(iLike('Jonas'));
+console.log(iLike('Maryte', 88));
+console.log(iLike('Ona', 66, 'balionus'));
